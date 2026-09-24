5e73e374c995c7635ddf6d593de7d9c2364b20ad
TheYamo <87617257+EpicYamo@users.noreply.github.com>
2026-09-24 21:51:12 +0300

    add intern files and basic implementation

--- ex03/Intern.cpp
+++ ex03/Intern.cpp
@@ -6,7 +6,24 @@
 /*   By: aaycan <aaycan@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/24 20:59:44 by aaycan            #+#    #+#             */
-/*   Updated: 2026/09/24 20:59:44 by aaycan           ###   ########.fr       */
+/*   Updated: 2026/09/24 21:50:59 by aaycan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "Intern.hpp"
+
+Intern::Intern() {}
+
+Intern::Intern(const Intern &other)
+{
+	(void)other;
+}
+
+Intern &Intern::operator=(const Intern &other)
+{
+	(void)other;
+	return (*this);
+}
+
+Intern::~Intern() {}
 
--- ex03/Intern.hpp
+++ ex03/Intern.hpp
@@ -6,7 +6,24 @@
 /*   By: aaycan <aaycan@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/24 20:59:41 by aaycan            #+#    #+#             */
-/*   Updated: 2026/09/24 20:59:42 by aaycan           ###   ########.fr       */
+/*   Updated: 2026/09/24 21:46:04 by aaycan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef INTERN_HPP
+# define INTERN_HPP
+
+# include "AForm.hpp"
+
+class Intern
+{
+	public:
+		Intern();
+		Intern(const Intern &other);
+		Intern &operator=(const Intern &other);
+		~Intern();
+
+		AForm	*makeForm(const std::string name, const std::string target) ;
+};
+
+#endif
